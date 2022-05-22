891b2ed5a9804c6fb5ceef4629faed6bf803d404
PayShop <cntt822002@gmail.com>
2022-05-22 18:48:58 +0700

    Add project files.

---
+++ RestAPI/Controllers/PagesController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestAPI.Models;
+
+namespace RestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PagesController : ControllerBase
+    {
+        private readonly PayShopContext context;
+
+        public PagesController(PayShopContext context)
+        {
+            this.context = context;
+        }
+
+        // GET /api/pages
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Page>>> GetPages()
+        {
+            return await context.Pages.OrderBy(x => x.Sorting).ToListAsync();
+        }
+
+        // GET /api/pages/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Page>> GetPage(int id)
+        {
+            var page = await context.Pages.FindAsync(id);
+
+            if (page == null)
+            {
+                return NotFound();
+            }
+
+            return page;
+        }
+
+        // PUT /api/pages/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Page>> PutPage(int id, Page page)
+        {
+            if (id != page.Id)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(page).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // POST /api/pages
+        [HttpPost]
+        public async Task<ActionResult<Page>> PostPage(Page page)
+        {
+            context.Pages.Add(page);
+            await context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(PostPage), page);
+        }
+
+        // DELETE /api/pages/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Page>> DeletePage(int id)
+        {
+            var page = await context.Pages.FindAsync(id);
+
+            context.Pages.Remove(page);
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ RestAPI/Models/PayShopContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPI.Models
+{
+    public class PayShopContext : DbContext
+    {
+        public PayShopContext(DbContextOptions<PayShopContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Page> Pages { get; set; }
+    }
+}
